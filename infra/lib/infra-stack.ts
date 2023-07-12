4bbd16dd5291d4306b0be9eac0d861cdee94c049
Alexandre Agius <aaagius@amazon.fr>
2023-07-12 16:37:54 +0200

    Update App Runner Connection ARN part

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,20 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import apprunner = require('@aws-cdk/aws-apprunner-alpha'); // Allows working with App Runner resources
-import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets'; // Allows building the docker image and uploading to ECR
-import * as path from "path"; // Helper for working with file paths
+import * as apprunnersdk from 'aws-sdk/clients/apprunner';
 
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const connectionName = 'your-connection-name';
+    const appRunnerClient = new apprunnersdk();
+
+    appRunnerClient.getConnection({ ConnectionName: connectionName }, (err, data) => {
+      if (err) {
+        console.error('Error retrieving connection ARN:', err);
+        return;
+      }
+
+      const connectionArn = data.Connection?.ConnectionArn;
+
     const service = new apprunner.Service(this, 'Service', {
       source: apprunner.Source.fromGitHub({
         repositoryUrl: 'https://github.com/agiusalexandre/minimal-dash-app',
         branch: 'main',
         configurationSource: apprunner.ConfigurationSourceType.REPOSITORY,
-        connection: apprunner.GitHubConnection.fromConnectionArn('arn:aws:apprunner:eu-west-1:284474675936:connection/AppRunner/39007be3e69c42508a464e1016d04004')
+        connection: apprunner.GitHubConnection.fromConnectionArn('YOUR_APP_RUNNER_CONNECTION_ARN')
       })
     });
 
